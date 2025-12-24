55ee200716a22c94a657f6621d7a3007788d32fd
Tanzilal Gummilang <tanzilalgummilang@gmail.com>
2025-12-24 19:10:38 +0700

    implement login

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Database\Seeders\UserSeeder;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    private string $name = 'John Doe';
+    private string $username = 'johndoe';
+    private string $password = 'test';
+    
     public function test_register_successfully()
     {
         $this->post('/api/users', [
@@ -66,6 +70,68 @@ class UserTest extends TestCase
             ->assertJson([
                 'errors' => [                    
                     'username' => ['The username has already been taken.'],
+                ]
+            ]);
+    }
+
+    public function test_login_successfully()
+    {
+        $this->seed(UserSeeder::class);
+        $this->post('/api/users/login', [
+            'username' => $this->username,
+            'password' => $this->password,
+        ])->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'name' => $this->name,
+                    'username' => $this->username,
+                ]
+            ]);
+
+        $user = User::query()->where('username', $this->username)->first();
+        self::assertNotNull($user->token);
+    }
+
+    public function test_login_username_not_found()
+    {
+        $this->seed(UserSeeder::class);
+        $this->post('/api/users/login', [
+            'username' => 'johnwick',
+            'password' => 'test',
+        ])->assertStatus(401)
+            ->assertJson([
+                'errors' => [
+                    'message' => ['username or password is wrong.'],
+                ]
+            ]);
+    }
+
+    public function test_login_password_wrong()
+    {
+        $this->seed(UserSeeder::class);
+        $this->post('/api/users/login', [
+            'username' => 'johndoe',
+            'password' => 'testing',
+        ])->assertStatus(401)
+            ->assertJson([
+                'errors' => [
+                    'message' => ['username or password is wrong.'],
+                ]
+            ]);
+    }
+
+    public function test_login_min_length()
+    {
+        $this->seed(UserSeeder::class);
+        $this->post('/api/users/login', [
+            'name' => 'John',
+            'username' => 'john',
+            'password' => 'pa',
+        ])->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'username' => ['The username field must be at least 6 characters.'],
+                    'password' => ['The password field must be at least 3 characters.'],
                 ]
             ]);
     }
